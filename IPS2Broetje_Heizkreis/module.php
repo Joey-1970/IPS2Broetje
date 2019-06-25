7bd75d829f2af15a63a45e6c52801ec239ec2eb2
Joey-1970 <paeper@horburg.de>
2019-06-25 21:11:10 +0200

    Update module.php

--- IPS2Broetje_Heizkreis/module.php
+++ IPS2Broetje_Heizkreis/module.php
@@ -187,36 +187,37 @@
 	
 	public function RequestAction($Ident, $Value) 
 	{
-  		switch($Ident) {
-	        case "Betriebsart":
-			If ($this->ReadPropertyBoolean("Open") == true) {
+  		If ($this->ReadPropertyBoolean("Open") == true) {
+			switch($Ident) {
+			case "Betriebsart":
 				SetData(1024, $Value);
-			}
-	            break;
-		case "Komfortsollwert":
-			If ($this->ReadPropertyBoolean("Open") == true) {
+			    break;
+			case "Komfortsollwert":
 				SetData(1025, ($Value * 64));
-			}
-	            break;
-		case "Reduziertsollwert":
-			If ($this->ReadPropertyBoolean("Open") == true) {
+			    break;
+			case "Reduziertsollwert":
 				SetData(1026, ($Value * 64));
-			}
-	            break;
-		case "Frostschutzsollwert":
-			If ($this->ReadPropertyBoolean("Open") == true) {
+			    break;
+			case "Frostschutzsollwert":
 				SetData(1027, ($Value * 64));
+			    break;
+			case "KennlinieSteilheit":
+				SetData(1028, ($Value * 50));
+			    break;
+			case "KennlinieVerschiebung":
+				SetData(1029, ($Value * 64));
+			    break;
+			case "SommerWinterheizgrenze":
+				SetData(1030, ($Value * 64));
+			    break;
+			case "Tagesheizgrenze":
+				SetData(1032, ($Value * 64));
+			    break;
+			default:
+			    throw new Exception("Invalid Ident");
 			}
-	            break;
-		case "KennlinieSteilheit":
-			If ($this->ReadPropertyBoolean("Open") == true) {
-				SetData(1027, ($Value * 50));
-			}
-	            break;
-	        default:
-	            throw new Exception("Invalid Ident");
-	    	}
-		$this->GetData();
+			$this->GetData();
+		}
 	}
 	    
 	public function ReceiveData($JSONString) 
