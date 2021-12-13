d341adb5e2df20fe3979d041f61d897845e253bf
Joey-1970 <paeper@horburg.de>
2021-12-13 13:33:29 +0100

    Update module.php

--- IPS2Broetje_Wasser/module.php
+++ IPS2Broetje_Wasser/module.php
@@ -186,7 +186,7 @@
 	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     	{
 		switch ($Message) {
-			case 10001:
+			case IPS_KERNELSTARTED:
 				// IPS_KERNELSTARTED
 				If ($this->ReadPropertyBoolean("Open") == true) {
 					$this->GetState();
